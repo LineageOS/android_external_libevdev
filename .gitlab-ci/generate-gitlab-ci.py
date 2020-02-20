f26679c6ebb39b732a965dba84f975260366d266
Peter Hutterer <peter.hutterer@who-t.net>
2020-02-21 09:05:04 +1000

    gitlab CI: move the configuration bits into a YAML file
    Only one change: the meson boolean to decide whether to build with meson is
    now inside the build: block.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- .gitlab-ci/generate-gitlab-ci.py
+++ .gitlab-ci/generate-gitlab-ci.py
@@ -7,53 +7,27 @@ import argparse
 import jinja2
 import os
 import sys
+import yaml
 
 from pathlib import Path
 
-
-distributions = [
-    {'name': 'fedora', 'version': '30'},
-    {'name': 'fedora', 'version': '31'},
-    {'name': 'ubuntu', 'version': '19.10'},
-    {'name': 'ubuntu', 'version': '19.04'},
-    {'name': 'debian', 'version': 'stable'},
-    {'name': 'debian', 'version': 'sid'},
-    {
-        'name': 'centos', 'version': '7',
-        'build': {
-            'extra_variables': {
-                'MAKE_ARGS': ('\'\'  # disable distcheck, requires doxygen'),
-            }
-        },
-        'meson': False
-    },
-    {
-        'name': 'centos', 'version': '8',
-        'build': {
-            'extra_variables': {
-                'MAKE_ARGS': ('\'\'  # disable distcheck, requires doxygen'),
-            }
-        },
-        'meson': False
-    },
-    {'name': 'arch', 'version': 'rolling',
-     'flavor': 'archlinux' },  # see https://gitlab.freedesktop.org/wayland/ci-templates/merge_requests/19
-    {'name': 'alpine', 'version': 'latest'},
-]
-
 # The various sources for templating
 SOURCE_DIR = Path('.gitlab-ci')
+CONFIG_FILE = 'gitlab-ci-config.yaml'
 TEMPLATE_FILE = 'gitlab-ci.tmpl'
 
 BASE_DIR = Path('.')
 OUTPUT_FILE = '.gitlab-ci.yml'
 
+
 def generate_template():
+    with open(SOURCE_DIR / CONFIG_FILE) as fd:
+        config = yaml.safe_load(fd)
+
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(os.fspath(SOURCE_DIR)),
                              trim_blocks=True, lstrip_blocks=True)
 
     template = env.get_template(TEMPLATE_FILE)
-    config = {'distributions': distributions}
     with open(BASE_DIR / OUTPUT_FILE, 'w') as fd:
         template.stream(config).dump(fd)
 
