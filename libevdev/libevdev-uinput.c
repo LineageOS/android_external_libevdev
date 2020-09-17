60d4f1b2aea14a65a4c2cfed7631fade405b7adf
Peter Hutterer <peter.hutterer@who-t.net>
2020-09-17 15:56:41 +1000

    Change to the (always intended) MIT license
    Due to what must've been a copy/paste error many years ago, the license text
    for libevdev wasn't actually the MIT license. Let's rectify this, it was
    always MIT intended anyway.
    
    To make this more obvious and reduce the chance of copy/paste mistakes, use
    the SPDX license identifier in the various source files. The two installed
    public header files have the full license text.
    
    All contributors with copyrightable contributions have ACKed the license
    change to MIT, either in the MR directly [1] or privately in reply to an
    email.
    
    [1] https://gitlab.freedesktop.org/libevdev/libevdev/-/merge_requests/69
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>
    Acked-by: Alexander Dahl <ada@thorsis.com>
    Acked-by: Andreas Pokorny <andreas.pokorny@canonical.com>
    Acked-by: Armin K <krejzi@email.com>
    Acked-by: Benjamin Tissoires <btissoir@redhat.com>
    Acked-by: David Herrmann <dh.herrmann@gmail.com>
    Acked-by: Deepa Dinamani <deepa.kernel@gmail.com>
    Acked-by: Emmanuele Bassi <ebassi@gnome.org>
    Acked-by: Gaetan Nadon <memsize@videotron.ca>
    Acked-by: George Thomas <georgefsthomas@gmail.com>
    Acked-by: Michael Forney <mforney@mforney.org>
    Acked-by: Nayan Deshmukh <nayan26deshmukh@gmail.com>
    Acked-by: Niclas Zeising <zeising@daemonic.se>
    Acked-by: Owen W. Taylor <otaylor@fishsoup.net>
    Acked-by: Peter Seiderer <ps.report@gmx.net>
    Acked-by: Ran Benita <ran234@gmail.com>
    Acked-by: Rosen Penev <rosenp@gmail.com>
    Acked-by: Scott Jann <sjann@knight-rider.org>
    Acked-by: Thilo Schulz <thilo@tjps.eu>
    Acked-by: polyphemus <rolfmorel@gmail.com>

--- libevdev/libevdev-uinput.c
+++ libevdev/libevdev-uinput.c
@@ -1,23 +1,6 @@
+// SPDX-License-Identifier: MIT
 /*
  * Copyright © 2013 Red Hat, Inc.
- *
- * Permission to use, copy, modify, distribute, and sell this software and its
- * documentation for any purpose is hereby granted without fee, provided that
- * the above copyright notice appear in all copies and that both that copyright
- * notice and this permission notice appear in supporting documentation, and
- * that the name of the copyright holders not be used in advertising or
- * publicity pertaining to distribution of the software without specific,
- * written prior permission.  The copyright holders make no representations
- * about the suitability of this software for any purpose.  It is provided "as
- * is" without express or implied warranty.
- *
- * THE COPYRIGHT HOLDERS DISCLAIM ALL WARRANTIES WITH REGARD TO THIS SOFTWARE,
- * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS, IN NO
- * EVENT SHALL THE COPYRIGHT HOLDERS BE LIABLE FOR ANY SPECIAL, INDIRECT OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE,
- * DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER
- * TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE
- * OF THIS SOFTWARE.
  */
 
 #include "config.h"
@@ -185,7 +168,7 @@ libevdev_uinput_get_fd(const struct libevdev_uinput *uinput_dev)
  * FreeBSD does not have anything similar to sysfs.
  * Set libevdev_uinput->syspath to NULL unconditionally.
  * Look up the device nodes directly instead of via sysfs, as this matches what
- * is returned by the UI_GET_SYSNAME ioctl() on FreeBSD.  
+ * is returned by the UI_GET_SYSNAME ioctl() on FreeBSD.
  */
 static int
 fetch_syspath_and_devnode(struct libevdev_uinput *uinput_dev)
